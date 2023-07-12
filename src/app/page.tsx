e560d193adc7c8f7c185ecd740a2d7aa28d23bb5
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-12 09:45:44 -0700

    Chat template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <Card className="w-[400px] h-[700px]">
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min_content]">
         <CardHeader>
           <CardTitle>Chat Ai</CardTitle>
           <CardDescription>Using Vercel SDK to create chat bot.</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Messages</p>
+        <CardContent className="space-y-3">
+          <div>
+            <Avatar>
+              <AvatarFallback>WC</AvatarFallback>
+              <AvatarImage src="https://github.com/wandersonce.png"/>
+            </Avatar>
+          </div>
+          <div>
+          <Avatar>
+              <AvatarFallback>BG</AvatarFallback>
+              <AvatarImage src="https://i.ibb.co/G73jXss/cc6821213daaca6520ee63ac94c3180c.jpg"/>
+          </Avatar>
+          </div>
         </CardContent>
-        <CardFooter>
-          Form
+        <CardFooter className="space-x-2">
+          <Input placeholder="How can I help you?"/>
+          <Button type="submit">Send</Button>
         </CardFooter>
       </Card>
     </div>
